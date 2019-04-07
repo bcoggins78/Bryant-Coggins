94f0fddb1e3aeb216d04ad2fbe9e7ce1e087b4f8
bcoggins78 <bcoggins78@gmail.com>
2019-04-07 11:48:37 -0400

    Added Navbar

---
+++ assets/javascript/navpane.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+
+    const navSlide = () => {
+        const burger = document.querySelector('.burger');
+        const nav = document.querySelector('.nav-links');
+        const navLinks = document.querySelectorAll('.nav-links li');
+
+        burger.addEventListener('click', () => {
+            // Opens the nav
+            nav.classList.toggle('nav-active');
+
+            // Animate the links
+            navLinks.forEach((link, index) => {
+                if (link.style.animation) {
+                    link.style.animation = '';
+                } else {
+                    link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+                }
+            });
+
+            // Burger animation
+            burger.classList.toggle('toggle');
+        });
+    }
+    navSlide();
+});
